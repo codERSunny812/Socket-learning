787ebeb2dbab0183a7a5ccdad22e53b9fac0727c
codersunny812 <sengersunny448@gmail.com>
2024-03-31 02:20:08 +0530

    server code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {Server} = require('socket.io');
 const {createServer} = require('http');
-const cors = require('cors')
+
 
 const PORT = 3004;
 
@@ -25,19 +25,35 @@ const io = new Server(server,{
 
 // This event is fired upon a new connection.
 io.on('connection', (socket) => {
+
     console.log("user connected with", socket.id);
 
     // new  event
     socket.emit("welcome", "Welcome to the chat server!");
 
     // broadcasting an event
+    socket.broadcast.emit("join", `${socket.id} has joined`);
+
+    // message event
+    socket.on('send_msg',(data)=>{
+    console.log(data);
     
+    // io.emit("receive_msg", data);
+   
+        // Emit the received message to all clients except the sender
+        
+        socket.broadcast.emit("receive_msg", data);
+    })
+
+
+
+    // disconnect listner
+    socket.on( 'disconnect', ()=>{
+    console.log(`user disconnect ${socket.id}`)});
 })
 
 
 
 server.listen(PORT,()=>{
     console.log(`server is listing at ${PORT}`)
-}
-
-)
+})
