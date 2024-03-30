8b8b4d514159d1fee427837ad657395a4b656774
codersunny812 <sengersunny448@gmail.com>
2024-03-31 02:19:49 +0530

    clinet code

--- client/socket.io/src/App.jsx
+++ client/socket.io/src/App.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import {io} from 'socket.io-client'
 
 const App = () => {
 
-  const socket = io('http://localhost:3004/');
+  const socket = useMemo(() => io('http://localhost:3004/'),[])
+
+  const [message , setMessage] = useState("");
 
  useEffect(()=>{
    socket.on('connect',()=>{
@@ -13,11 +15,39 @@ const App = () => {
    socket.on("welcome",(data)=>{
     console.log(data);
    })
+
+   socket.on("join",(data)=>{
+    console.log(data);
+   })
+
+   socket.on("receive_msg",(data)=>{
+    console.log(data);
+   })
+
+   return () =>{
+    socket.disconnect();
+   }
  },[socket])
+
+ const handleSubmit = (e)=>{
+e.preventDefault();
+
+socket.emit( "send_msg" , message );
+setMessage("");
+
+ }
 
   return (
     <div>
       <h1>socket  learning</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea name="" id="" cols="30" rows="6"
+        placeholder='type your message'
+        value={message}
+        onChange={(e)=> setMessage(e.target.value)}
+        ></textarea>
+        <button type="submit">send</button>
+      </form>
     </div>
   )
 }
